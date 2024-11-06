4df3967e8365b74622a2ee4e71e23f3fb6b1abb5
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-11-06 18:09:17 +0000

    sm6225-common: Update blobs to LA.UM.9.15.2.r1-10600-KAMORTA.QSSI14.0
    Change-Id: I27778ecb9a573507b7ad73b859a3396f63a507fc
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -69,7 +69,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
             [ "$2" = "" ] && return 0
             [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
